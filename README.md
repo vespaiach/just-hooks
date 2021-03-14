bab42f976cad8474c5b1a501d38bd98a88e07aac
Toan Nguyen <nta.toan@gmail.com>
2021-03-14 18:40:52 -0400

    Add useTransferList

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # just-hooks
-Collection of useful React Hooks
+
+Just a collection of useful React Hooks.
+
+## Transfering List
+
+Given two lists `start` and `end` and we want to transfer their items back and forth.
+
+```
+const {
+  start,
+  end,
+  add,
+  remove
+} = useTransferList(initialList, equalityCompareFn)
+```
+
+- start: start list (can be list of number, string or object that has an id key)
+- end: end list (can be list of number, string or object that has an id key)
+- add: add() function will move item from start to end
+- remove: remove() function will move item back from end to start
+- equalityCompareFn: is optional. This function will compare item in list with item being added or removed to see if they are matched
---
+++ src/useTransferList.ts
@@ -0,0 +1,76 @@
+import { useCallback, useState } from 'react';
+
+const defaultEqualityCompare: (item: any, addOrRemoveItem: any) => boolean = (
+  item,
+  addOrRemoveItem
+) => {
+  const typeofItem = typeof item;
+  const typeofAddOrRemoveItem = typeof addOrRemoveItem;
+
+  if (
+    (typeofItem === 'number' && typeofAddOrRemoveItem === 'number') ||
+    (typeofItem === 'string' && typeofAddOrRemoveItem === 'string')
+  ) {
+    return item === addOrRemoveItem;
+  }
+
+  return item.id === addOrRemoveItem.id;
+};
+
+export interface KeyedObject {
+  id: string | number;
+}
+
+/**
+ *
+ * @param initialList list of initial items. Item's type can be number, string or object that has an `id` field
+ * @param equalityCompareFn (optional) comparing function filter out items that are removed from lists. Return true will keep items or false to remove them
+ * @returns
+ * {
+ *   start: list of initial items,
+ *   end: list of items that have been transfered to,
+ *   add(): transfer item to end list,
+ *   remove(): transfer item back to start list
+ * }
+ */
+export default function useTransferList<
+  T extends number | string | KeyedObject
+>(
+  initialList: T[],
+  equalityCompareFn: (
+    item: T,
+    addOrRemoveItem: T
+  ) => boolean = defaultEqualityCompare
+) {
+  const [start, setStart] = useState<T[]>(initialList);
+  const [end, setEnd] = useState<T[]>([]);
+
+  const add = useCallback(
+    (item: T) => {
+      const index = start.findIndex(it => equalityCompareFn(it, item));
+      if (index > -1) {
+        setStart(start.filter(it => !equalityCompareFn(item, it)));
+        setEnd([...end, item]);
+      }
+    },
+    [setStart, setEnd, start, end]
+  );
+
+  const remove = useCallback(
+    (item: T) => {
+      const index = end.findIndex(it => equalityCompareFn(it, item));
+      if (index > -1) {
+        setStart([...start, item]);
+        setEnd(end.filter(it => !equalityCompareFn(item, it)));
+      }
+    },
+    [setStart, setEnd, start, end]
+  );
+
+  return {
+    start,
+    end,
+    add,
+    remove,
+  };
+}
